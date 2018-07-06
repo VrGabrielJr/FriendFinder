1e9a180a52377364c14c438c36336cf97cd14fcb
Val Gabriel <gabriel.valryan@hotmail.com>
2018-07-06 16:50:30 -0500

    Added 1 friends data to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsData = [
+    {
+        name: "Ryan Gabriel",
+        image: "https://media.licdn.com/dms/image/C4D03AQGzNpJLQ8La5w/profile-displayphoto-shrink_200_200/0?e=1536192000&v=beta&t=s4ZOirTwOTevifk_mohYLK8zxgwsfKWTWt3CBgo3bak",
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4" 
+        ]
+    }
+]
+
+module.exports = friendsData;
