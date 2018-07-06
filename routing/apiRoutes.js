aa62b16167cbbe4c7798e445d5c0fc279c96c0ea
Val Gabriel <gabriel.valryan@hotmail.com>
2018-07-06 16:55:07 -0500

    Finish Logic

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -4,6 +4,34 @@ module.exports = function(app) {
     });
 
     app.post('/api/friends', function (req, res) {
-        
-    })
+        var newFriendScores = req.body.scores;
+        var scoresArr = [];
+        var match = 0;
+
+    //runs through all current friends in list
+    for(var i=0; i<friendsData.length; i++){
+      var scoresDiff = 0;
+      //run through scores to compare friends
+      for(var j=0; j<newFriendScores.length; j++){
+        scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
+
+      //push results into scoresArray
+      scoresArr.push(scoresDiff);
+    }
+
+    //after all friends are compared, find best match
+    for(var i=0; i<scoresArr.length; i++){
+      if(scoresArray[i] <= scoresArray[match]){
+        match = i;
+      }
+    }
+
+    //return bestMatch data
+    var bestMatch = friendList[match];
+    res.json(bestMatch);
+
+    //pushes new submission into the friendsList array
+    friendsData.push(req.body);
+    });
 };
