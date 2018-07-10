8effd70ea8dddd42020789b4ba0b82dfefc1b81e
Val Gabriel <gabriel.valryan@hotmail.com>
2018-07-09 19:22:17 -0500

    Finished

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsData = [
     {
-        name: "Ryan Gabriel",
-        image: "https://media.licdn.com/dms/image/C4D03AQGzNpJLQ8La5w/profile-displayphoto-shrink_200_200/0?e=1536192000&v=beta&t=s4ZOirTwOTevifk_mohYLK8zxgwsfKWTWt3CBgo3bak",
+        name: "Joe Navy",
+        image: "http://i76.photobucket.com/albums/j33/ritter4life/Buddy.jpg",
         scores: [
             "4",
             "5",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,5 @@
+var friendsData = require("../data/friends.js");
+
 module.exports = function(app) {
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
@@ -8,30 +10,26 @@ module.exports = function(app) {
         var scoresArr = [];
         var match = 0;
 
-    //runs through all current friends in list
     for(var i=0; i<friendsData.length; i++){
       var scoresDiff = 0;
-      //run through scores to compare friends
+
       for(var j=0; j<newFriendScores.length; j++){
         scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendScores[j])));
       }
 
-      //push results into scoresArray
       scoresArr.push(scoresDiff);
     }
 
-    //after all friends are compared, find best match
+
     for(var i=0; i<scoresArr.length; i++){
-      if(scoresArray[i] <= scoresArray[match]){
+      if(scoresArr[i] <= scoresArr[match]){
         match = i;
       }
     }
 
-    //return bestMatch data
-    var bestMatch = friendList[match];
+    var bestMatch = friendsData[match];
     res.json(bestMatch);
 
-    //pushes new submission into the friendsList array
     friendsData.push(req.body);
     });
 };
